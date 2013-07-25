cd9f33ce3883859365009ffbc9f833a1af987b38
rascarlo <rascarlo@gmail.com>
2013-07-25 11:52:59 +0200

    use Shaula as default notification

--- target/product/core.mk
+++ target/product/core.mk
@@ -20,7 +20,7 @@
 # core_minimal.mk instead.
 
 PRODUCT_PROPERTY_OVERRIDES := \
-    ro.config.notification_sound=OnTheHunt.ogg \
+    ro.config.notification_sound=Shaula.ogg \
     ro.config.alarm_alert=Alarm_Classic.ogg
 
 PRODUCT_PACKAGES += \
